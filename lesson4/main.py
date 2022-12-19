3ad5ab03a730fcfd850b5ff12dd68c1c317e86c1
lesson 3 <theantek204@gmail.com>
2022-12-19 23:14:01 +0200

    add main.py file

---
+++ lesson4/main.py
@@ -0,0 +1,9 @@
+a=int(input('Enter first number: '))
+b=int(input('Enter second number: '))
+c=int(input('Enter third number: '))
+if a > 10 and b > 10 and c > 10:
+    print('Yes')
+else:
+    print('No')
+
+
