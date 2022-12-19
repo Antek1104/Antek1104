28173530e8142e37e4bb5f25604d63956e5c77be
lesson 3 <theantek204@gmail.com>
2022-12-19 18:37:02 +0200

    add main.py file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# задание 1
 a=int(input())
 b=int(input())
 c=int(input())
@@ -5,5 +6,15 @@ if a > 10 and b > 10 and c > 10:
     print("Yes")
 else:
     print("No")
+
+#задание 2
+a, b, c = int(input()), int(input()), int(input())
+mx = a
+if b > mx:
+    mx = b
+if c > mx:
+    mx = c
+print(mx)
+
 
 
