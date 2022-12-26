7366a9ad73c652d13079429434a75cd0035913e0
lesson 3 <theantek204@gmail.com>
2022-12-26 19:27:08 +0200

    commit task5.py file

--- lesson6/task5.py
+++ lesson6/task5.py
@@ -1,16 +1,16 @@
 # Проверка 5:
 # В пароле есть хотя бы один спец символ (+, -, /, _, % и т.д. P.S. их на самом деле больше)
 
-password = 'adMin123\t'
+password = 'adMin123@#'
 has_digit = False
 
 for s in password:
     print('Good')
-    if s.isspace():
+    if s.punctuation():
         has_digit = True
         break
 
-if s.isspace():
+if s.punctuation():
     print('Skore:5 ')
 else:
     print('-')
