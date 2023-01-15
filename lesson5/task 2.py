92b9b2e41c74f252e314e16c47156506d81cceb9
Antek1104 <120098684+Antek1104@users.noreply.github.com>
2023-01-15 18:38:54 +0200

    Update task 2.py

--- lesson5/task 2.py
+++ lesson5/task 2.py
@@ -1,12 +1,10 @@
 # Вывести треугольник #2 с шириной N с помощью цикла while
 
-N = int(input('Ввести ширину треугольника: '))
-N = 1
-d = '*'
-print(N * d)
-while N != 5:
-        N += 1
-        print(N * d)
+a = int(input('Enter N number:'))
+i = 0
+while i <= a:
+    print('*'*i)
+    i += 1
 
 
     
