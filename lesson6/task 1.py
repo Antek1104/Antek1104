7e0445c88137492cd1860d27482f7275038599cf
Antek1104 <120098684+Antek1104@users.noreply.github.com>
2023-01-22 16:27:27 +0200

    Update task 1.py

--- lesson6/task 1.py
+++ lesson6/task 1.py
@@ -15,46 +15,48 @@ has_lower = False
 has_upper = False
 
 for i in password:
-if i.isupper():
-has_upper = True
-break
+    if i.isupper():
+     has_upper = True
+    break
 if has_upper:
-count += 1
+    count += 1
 else:
-print('Добавьте букву верхнего регистра')
+    print('Добавьте букву верхнего регистра')
 
 for i in password:
-if i.islower():
-has_lower = True
-break
+    if i.islower():
+     has_lower = True
+    break
 if has_lower:
-count += 1
+    count += 1
 else:
-print('Добавьте букву нижнего регистра')
+    print('Добавьте букву нижнего регистра')
 
 for i in password:
-if i.isdigit():
-has_digit = True
-break
+    if i.isdigit():
+     has_digit = True
+    break
 if has_digit:
-count += 1
+    count += 1
 else:
-print('Добавьте цифру в Ваш пароль')
+    print('Добавьте цифру в Ваш пароль')
 
 for i in password:
-if i in '!@#$%^&*()_+?><|"':
-has_special = True
-break
-if has_special:
-count += 1
+    if i in '!@#$%^&*()_+?><|"':
+     has_special = True
+    break
+    if has_special:
+     count += 1
 else:
-print('Добавьте спец символ в Ваш пароль')
+    print('Добавьте спец символ в Ваш пароль')
 
 if len(password) >= 8:
-count += 1
+    count += 1
 else:
-print('Длина Вашего пароля менее 8 исмволов')
+    print('Длина Вашего пароля менее 8 исмволов')
 
-print(f'Ваш пароль получил {count} балов из 5')
+    print(f'Ваш пароль получил {count} балов из 5')
+
+
 
 
